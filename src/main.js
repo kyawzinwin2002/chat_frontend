a0dec11cfd4a7cfa82a93655934f3ae1506c8040
kyawzinwin2002 <kyawzinw469@gmail.com>
2023-12-18 20:49:48 +0630

    addTailwindAndFlowBite

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import axios from "axios"
+
+axios.defaults.baseURL = "http://localhost:8000/api/v1";
+axios.defaults.headers.common["Accept"] = "application/json";
 
 createApp(App).mount('#app')
